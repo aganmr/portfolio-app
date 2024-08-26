184056d9ccf4f6990b838c97737f322caf09ae36
aganmr <markagan07@gmail.com>
2024-08-27 00:49:02 +0800

    added image path

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           style={{ width: '100%', height: 'auto' }}
-          src="/header.svg"
+          src="/portfolio-app/header.svg"
           alt="header"
           width={0}
           height={0}
@@ -19,7 +19,7 @@ export default function Home() {
         <a href="https://github.com/markurei">
           <Image
             style={{ width: '100%', height: 'auto', marginTop: -10, marginBottom: -5 }}
-            src="/github_3.svg"
+            src="/portfolio-app/github_3.svg"
             alt="github"
             width={0}
             height={0}
@@ -30,7 +30,7 @@ export default function Home() {
       <div>
         <Image
           style={{ width: '100%', height: 'auto' }}
-          src="/body_1.svg"
+          src="/portfolio-app/body_1.svg"
           alt="body 1"
           width={0}
           height={0}
@@ -41,7 +41,7 @@ export default function Home() {
         <a href="https://play.google.com/store/apps/details?id=com.spingine.xeleqt.tracker&hl=en_US">
           <Image
             style={{ width: '100%', height: 'auto', marginTop: -12, marginBottom: -5 }}
-            src="/playstore_3.svg"
+            src="/portfolio-app/playstore_3.svg"
             alt="playstore"
             width={0}
             height={0}
@@ -52,7 +52,7 @@ export default function Home() {
       <div>
         <Image
           style={{ width: '100%', height: 'auto' }} 
-          src="/body_2.svg"
+          src="/portfolio-app/body_2.svg"
           alt="body 2"
           width={0}
           height={0}
@@ -63,7 +63,7 @@ export default function Home() {
         <a href="mailto:someone@markagan07@gmail.com">
           <Image
             style={{ width: '100%', height: 'auto', marginTop: -12, marginBottom: -5 }}
-            src="/gmail_1.svg"
+            src="/portfolio-app/gmail_1.svg"
             alt="gmail"
             width={0}
             height={0}
@@ -75,7 +75,7 @@ export default function Home() {
         <a href="https://ph.linkedin.com/in/mark-rey-agan">
           <Image
             style={{ width: '100%', height: 'auto', marginTop: -5 }}
-            src="/linkedin_1.svg"
+            src="/portfolio-app/linkedin_1.svg"
             alt="linkedin"
             width={0}
             height={0}
@@ -83,21 +83,10 @@ export default function Home() {
           />
         </a>
       </div>
-      {/* <div style={{ width: '100%', height: 'auto', backgroundColor: '#313542', marginTop: -5, maxWidth: '1080px', paddingTop: 4}}>
-        <a href="https://ph.linkedin.com/in/mark-rey-agan" style={{ marginLeft: 0 }}>
-          <Image
-            src="/linkedin_link.svg"
-            alt="linkedin"
-            width={width * 0.23}
-            height={23}
-            priority
-          />
-        </a>
-      </div> */}
       <div>
         <Image
           style={{ width: '100%', height: 'auto',marginTop: -7 }}
-          src="/footer.svg"
+          src="/portfolio-app/footer.svg"
           alt="footer"
           width={0}
           height={0}
